7720bd8d2eb3009795b1707e2a9884c1295616ed
adityasw <adityasw65@gmail.com>
2025-05-05 13:09:56 +0530

    form created for material UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormForMaterialUIComponent } from './form-for-material-ui/form-for-material-ui.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormForMaterialUIComponent
   ],
   imports: [
     BrowserModule,
